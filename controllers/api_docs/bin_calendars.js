e8afe7d083438799cd7152a9861b9804bf6aa662
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-10-24 15:22:08 +0000

    Build a method for creating calendar Object

--- controllers/api_docs/bin_calendars.js
+++ controllers/api_docs/bin_calendars.js
@@ -1,6 +1,9 @@
 import { BinCalendarSrc } from '../../config/api_urls.js'
 import fs from 'fs'
 
+const weekday_nums = { '日': 0, '月': 1, '火': 2, '水': 3, '木': 4, '金': 5, '土': 6 }
+const month_dates = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
 const linearSearch = (arr, key_name) => {
    let name = new RegExp(key_name)
    for(let i = 0; i < arr.length; i++){
@@ -10,15 +13,96 @@ const linearSearch = (arr, key_name) => {
    }
 }
 
-const createCalendar = () => {
-   
+const createCalendar = (weekdays, init_date) => {
+
+   let bin_days = new Array(7)
+   for(let i = 0; i < bin_days.length; i++){
+      bin_days[i] = new Array()
+   }
+
+   for(let i = 0; i < weekdays.length; i++){
+      let weekday = weekdays[i]
+      let pos = weekday_nums[weekday.date]
+      bin_days[pos].push({freq: weekday.freq, bin: weekday.bin})
+   }
+
+   let calendar = new Object()
+   let year = init_date.getFullYear()
+   let month = init_date.getMonth()
+   let days = new Array()
+
+   /*
+      JSON structure could be like this.
+      {
+         year: 2017,
+         month: 10,
+         days: [
+            bins: [
+               {
+                  type: "焼却ゴミ類",
+                  color: "blue"
+               }
+            ],
+            bins: []
+         ]
+      }
+   */
+
+   const formatDate = (date, day) => {
+      let target_date = new Date(date.setDate(day))
+      let y = target_date.getFullYear()
+      let m = target_date.getMonth()
+      let d = target_date.getDate()
+      return Date.parse(new Date(y, m, d))
+   }
+
+   let date = new Date(new Date().setMonth(month))
+   let first_day = new Date(date.setDate(1)).getDay()
+   let max_days = month_dates[month]
+
+   // Looped by days in one month
+   for(let i = 0; i < max_days; i++){
+      let bins = new Array()
+      // Looped by an array that stores bin data in each weekdays
+      if(month == 0 && i < 3){
+         days.push(bins)
+         continue
+      }
+
+      for(let j = 0; j < bin_days.length; j++){
+         let current_day = (i + first_day) % 7
+         if(current_day == j && bin_days[j].length > 0){
+            // Looped by linear list (like moving veritcally)
+            for(let k = 0; k < bin_days[j].length; k++){
+               let bin_data = bin_days[j][k]
+               let week
+               if(month == 0){
+                   week = Math.ceil((i - 2) / 7)
+               }else{
+                   week = Math.ceil(i / 7)
+               }
+               if( week == bin_data.freq || bin_data.freq == 0){
+                  bins.push(bin_data.bin)
+               }
+            }
+            break
+         }
+      }
+      days.push(bins)
+   }
+
+   calendar = { year: year, month: month + 1, days: days }
+   return calendar
 }
 
-const getBinCalendars = (area_name) => {
+const getBinCalendars = () => {
    return new Promise((resolve, reject) => {
-      fs.readFile('./models/fixtures/jsons/bin_calendars/Shiga-ken/kusatsu/kusatsu.json', 'utf-8', (err, data) => {
+      fs.readFile('./models/fixtures/jsons/bin_calendars/滋賀県/草津市/草津市.json', 'utf-8', (err, data) => {
          let parsed_data = JSON.parse(data)
-         resolve(linearSearch(parsed_data, area_name))
+         let area = linearSearch(parsed_data.areas, '西渋川')
+         let calendar = createCalendar(area.weekdays, new Date(new Date().setMonth(0)))
+
+         resolve({ name: area.name, calendar: calendar })
       })
    }).then((value) => {
       return value
